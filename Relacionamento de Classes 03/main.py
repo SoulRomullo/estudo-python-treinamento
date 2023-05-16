4fb4ce40bf829c11bec13466207ac672e95e8419
SoulRomullo <soulromullo@gmail.com>
2023-05-16 11:31:30 -0300

    feat: class this completed

---
+++ Relacionamento de Classes 03/main.py
@@ -0,0 +1,32 @@
+from classes import Cliente
+
+cliente1 = Cliente('Romullo', 37)
+cliente1.insere_endereco('São Paulo', 'SP')
+print(cliente1.nome)
+cliente1.lista_endereco()
+# del(cliente1) # Usando este del ele informa com o primeiro print que foi exibido os dados inseridos e logo esse del
+# informa que o que inserido foi apagado da memória
+print()
+
+cliente2 = Cliente('Meire', 55)
+cliente2.insere_endereco('São Vicente', 'SP')
+cliente2.insere_endereco('Rio de Janeiro', 'RJ')
+print(cliente2.nome)
+cliente2.lista_endereco()
+# del(cliente2) # Usando este del ele informa com o primeiro print que foi exibido os dados inseridos e logo esse del
+# informa que o que inserido foi apagado da memória
+
+print()
+
+cliente3 = Cliente('Zilda', 75)
+cliente3.insere_endereco('Itabuna', 'BA')
+print(cliente3.nome)
+cliente3.lista_endereco()
+# del(cliente3) # Usando este del ele informa com o primeiro print que foi exibido os dados inseridos e logo esse del
+# informa que o que inserido foi apagado da memória
+
+print()
+
+print('##########################################')
+
+
